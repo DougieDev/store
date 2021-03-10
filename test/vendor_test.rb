ff6938b51eccaa6ca3d023dacafd0153f028e56c
Carlyle Douglas <cdouglascorp@gmail.com>
2021-03-10 09:13:39 -0700

    Add test for instance of Vendor

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -4,4 +4,11 @@ require './lib/item'
 require './lib/vendor'
 
 class VendorTest < Minitest::Test
+  def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
 end
