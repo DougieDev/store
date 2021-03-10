0e5c90fc5438df4060f538ac9606c5a39b28fc98
Carlyle Douglas <cdouglascorp@gmail.com>
2021-03-10 09:51:45 -0700

    Add test for adding vendors and vendor names

--- test/market_test.rb
+++ test/market_test.rb
@@ -8,6 +8,13 @@ require 'pry'
 class MarketTest < Minitest::Test
   def setup
     @market = Market.new("South Pearl Street Farmers Market")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
   end
 
   def test_it_exists
@@ -20,5 +27,21 @@ class MarketTest < Minitest::Test
 
   def test_it_starts_with_no_vendors
     assert_equal [], @market.vendors
+  end
+
+  def test_it_can_add_vendors
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal [@vendor1, @vendor2, @vendor3], @market.vendors
+  end
+
+  def test_it_can_return_vendor_names
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @market.vendor_names
   end
 end
