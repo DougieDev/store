0fc2ce3b1c64d19cf88be43662607f54e50f1488
Carlyle Douglas <cdouglascorp@gmail.com>
2021-03-10 10:51:17 -0700

    Add test to sort items by name

--- test/market_test.rb
+++ test/market_test.rb
@@ -15,6 +15,7 @@ class MarketTest < Minitest::Test
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @item5 = Item.new({name: 'Onion', price: '$0.25'})
   end
 
   def test_it_exists
@@ -107,5 +108,22 @@ class MarketTest < Minitest::Test
     @vendor3.stock(@item3, 10)
 
     assert_equal [@item1], @market.overstocked_items
+  end
+
+  def test_sorted_items
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
+
+    sorted_list = ["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"]
+
+    assert_equal sorted_list, @market.sorted_items_list
   end
 end
